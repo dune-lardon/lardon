b40891f72753a596ec5ef05c6c9d7a967169b212
Laura Zambelli <laura.zambelli@lapp.in2p3.fr>
2021-11-23 14:26:58 +0100

    small typos

--- config.py
+++ config.py
@@ -32,8 +32,8 @@ ADC_to_fC = 0
 elec = "none"
 channel_map = ""
 broken_channels = []
-x_offset = 0.
-y_offset = 0.
+view_offset = []
+
 
 
 
--- det_spec.py
+++ det_spec.py
@@ -9,7 +9,7 @@ def configure(detector, period, elec, run):
         cf.n_view = int(data['n_view'])
         cf.view_name = data['view_name']
         cf.view_type = data['view_type']
-        cf.view_angle = [int(x) for x in data['view_angle']]
+        cf.view_angle = [float(x) for x in data['view_angle']]
         cf.view_pitch = [float(x) for x in data['view_pitch']]
         cf.view_nchan = [int(x) for x in data['view_nchan']]
         cf.view_capa  = [float(x) for x in data['view_capa']]
@@ -19,8 +19,8 @@ def configure(detector, period, elec, run):
         cf.sampling = float(data[elec]['sampling'])
         cf.ADC_to_fC = float(data[elec]['ADC_to_fC'])
         cf.data_path += "/" + data[elec]['sub_path']
-        cf.x_offset = float(data[elec]['x_offset'])
-        cf.y_offset = float(data[elec]['y_offset'])
+        cf.view_offset = [float(x) for x in data[elec]['view_offset']]
+
 
     """ shorter term parameters """
     with open('settings/run_'+detector+'.json','r') as f:
