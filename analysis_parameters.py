d9c3cd934ec06d03273c9790d5ca2972b324e5bc
Yoann Kermaidic <ykermaid@lxplus727.cern.ch>
2021-11-29 10:55:25 +0100

    Add more analysis parameters in JSON file - revert numba stuff

--- analysis_parameters.py
+++ analysis_parameters.py
@@ -1,16 +1,31 @@
 import json as json
 
+# To validate JSON file: https://jsonlint.com
+
 class params:
     def __init__(self):
        self.ped_amp_sig_fst = 1          # default amplitude trigger threshold for 1st pass signal mask - in RMS 
        self.ped_amp_sig_oth = 1          # default amplitude trigger threshold for other pass signal mask - in RMS 
+
+       self.noise_coh_group = [32]       # coherent noise channel grouping
+       self.noise_fft_freq  = -1         # ??
+       self.noise_fft_lcut  = 0.0225     # low-pass filter frequency cut
 
        self.hit_amp_sig     = [3,6,2]    # default amplitude trigger threshold for hit search - in RMS
        self.hit_dt_min      = [10,10,10] # minimal delta t for hit search - in bins
        self.hit_pad_left    = 6
        self.hit_pad_right   = 10
 
-    def read(self,config="1"):
+       self.plt_noise_show        = 0
+       self.plt_evt_disp_daq_show = 0
+       self.plt_evt_disp_vch_show = 0
+
+       self.plt_noise_zrange= [0,900]                # color scale for noise plots
+       self.plt_evt_disp_daq_zrange = [-1000,1000]   # color scale for DAQ channels    event display plots
+       self.plt_evt_disp_vch_ind_zrange = [-100,100] # color scale for induction  view event display plots
+       self.plt_evt_disp_vch_col_zrange = [-50,50]   # color scale for collection view event display plots
+
+    def read(self,elec="top",config="1"):
        try:
           with open('settings/analysis_parameters.json','r') as f:
              data = json.load(f)
@@ -18,13 +33,25 @@ class params:
                 print("WARNING: Thresholds configuration ",config," not found.")
                 print("         Default thresholds will be applied.")
              else:
-                self.ped_amp_sig_fst = data[config]['pedestal']['first_pass_thrs']
-                self.ped_amp_sig_oth = data[config]['pedestal']['other_pass_thrs']
+                self.ped_amp_sig_fst = data[config][elec]['pedestal']['first_pass_thrs']
+                self.ped_amp_sig_oth = data[config][elec]['pedestal']['other_pass_thrs']
 
-                self.hit_amp_sig     = data[config]['hit_finder']['amp_sig_thrs']
-                self.hit_dt_min      = data[config]['hit_finder']['dt_min']
-                self.hit_pad_left    = data[config]['hit_finder']['pad']['left']
-                self.hit_pad_right   = data[config]['hit_finder']['pad']['right']
+                self.noise_coh_group = data[config][elec]['noise']['coherent']['groupings']
+                self.noise_fft_freq  = data[config][elec]['noise']['fft']['freq']
+                self.noise_fft_lcut  = data[config][elec]['noise']['fft']['low_cut']
+
+                self.hit_amp_sig     = data[config][elec]['hit_finder']['amp_sig_thrs']
+                self.hit_dt_min      = data[config][elec]['hit_finder']['dt_min']
+                self.hit_pad_left    = data[config][elec]['hit_finder']['pad']['left']
+                self.hit_pad_right   = data[config][elec]['hit_finder']['pad']['right']
+
+                self.plt_noise_show              = data[config][elec]['plot']['noise']['show']
+                self.plt_noise_zrange            = data[config][elec]['plot']['noise']['zrange']
+                self.plt_evt_disp_daq_show       = data[config][elec]['plot']['evt_display']['daqch']['show']
+                self.plt_evt_disp_daq_zrange     = data[config][elec]['plot']['evt_display']['daqch']['zrange']
+                self.plt_evt_disp_vch_show       = data[config][elec]['plot']['evt_display']['viewch']['show']
+                self.plt_evt_disp_vch_ind_zrange = data[config][elec]['plot']['evt_display']['viewch']['ind_zrange']
+                self.plt_evt_disp_vch_col_zrange = data[config][elec]['plot']['evt_display']['viewch']['col_zrange']
        except:
             print("WARNING: Thresholds setting file (./settings/analysis_parameters.json) not found.")
             print("         Default thresholds will be applied.")
