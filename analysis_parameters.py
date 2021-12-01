c5872f9348251041bdd3a83ff26ad098ec23d781
Yoann Kermaidic <ykermaid@lxplus784.cern.ch>
2021-12-01 11:53:22 +0100

    Add thresholds for refine_mask()

--- analysis_parameters.py
+++ analysis_parameters.py
@@ -1,4 +1,5 @@
 import json as json
+import sys
 
 # To validate JSON file: https://jsonlint.com
 
@@ -6,6 +7,11 @@ class params:
     def __init__(self):
        self.ped_amp_sig_fst = 1          # default amplitude trigger threshold for 1st pass signal mask - in RMS 
        self.ped_amp_sig_oth = 1          # default amplitude trigger threshold for other pass signal mask - in RMS 
+       self.ped_rise_thr = [5,5,30]      # consecutive positive samples threshold
+       self.ped_ramp_thr = [1,1,2]       # consecutive increasing sample threshold
+       self.ped_amp_thr  = [2,2,3]       # RMS amplitude threshold
+       self.ped_dt_thr   = 100           # trigger window duration in which a signal is looked for
+       self.ped_zero_cross_thr = 15      # minimal number of samples after downward zero-crossing to look for upward zero-crossing
 
        self.noise_coh_group = [32]       # coherent noise channel grouping
        self.noise_fft_freq  = -1         # ??
@@ -13,8 +19,8 @@ class params:
 
        self.hit_amp_sig     = [3,6,2]    # default amplitude trigger threshold for hit search - in RMS
        self.hit_dt_min      = [10,10,10] # minimal delta t for hit search - in bins
-       self.hit_pad_left    = 6
-       self.hit_pad_right   = 10
+       self.hit_pad_left    = 6          # hit lower side band
+       self.hit_pad_right   = 10         # hit upper side band
 
        self.plt_noise_show        = 0
        self.plt_evt_disp_daq_show = 0
@@ -26,15 +32,21 @@ class params:
        self.plt_evt_disp_vch_col_zrange = [-50,50]   # color scale for collection view event display plots
 
     def read(self,elec="top",config="1"):
-       try:
-          with open('settings/analysis_parameters.json','r') as f:
+       with open('settings/analysis_parameters.json','r') as f:
+             print("AnalysisParameters: Loading analysis setting file: settings/analysis_parameters.json ... ", end='')
              data = json.load(f)
+             print("done")
              if(config not in data):
-                print("WARNING: Thresholds configuration ",config," not found.")
+                print("WARNING: Analysis configuration ",config," not found.")
                 print("         Default thresholds will be applied.")
              else:
                 self.ped_amp_sig_fst = data[config][elec]['pedestal']['first_pass_thrs']
                 self.ped_amp_sig_oth = data[config][elec]['pedestal']['other_pass_thrs']
+                self.ped_rise_thr    = data[config][elec]['pedestal']['rise_thr']
+                self.ped_ramp_thr    = data[config][elec]['pedestal']['ramp_thr']
+                self.ped_amp_thr     = data[config][elec]['pedestal']['amp_thr']
+                self.ped_dt_thr      = data[config][elec]['pedestal']['dt_thr']
+                self.ped_zero_cross_thr = data[config][elec]['pedestal']['zero_cross_thr']
 
                 self.noise_coh_group = data[config][elec]['noise']['coherent']['groupings']
                 self.noise_fft_freq  = data[config][elec]['noise']['fft']['freq']
@@ -52,9 +64,6 @@ class params:
                 self.plt_evt_disp_vch_show       = data[config][elec]['plot']['evt_display']['viewch']['show']
                 self.plt_evt_disp_vch_ind_zrange = data[config][elec]['plot']['evt_display']['viewch']['ind_zrange']
                 self.plt_evt_disp_vch_col_zrange = data[config][elec]['plot']['evt_display']['viewch']['col_zrange']
-       except:
-            print("WARNING: Thresholds setting file (./settings/analysis_parameters.json) not found.")
-            print("         Default thresholds will be applied.")
 
     # setters and getters (potentially not useful now)
     def set_ped_amp_sig_fst(self,value):
