ds computation in induction views bug fixed

track_3d.py
@@ -125,20 +125,11 @@ def complete_trajectories(tracks):
         v_other = other.view
         ang_other = np.radians(cf.view_angle[v_other])
 
-        A = np.array([[np.cos(ang_track), -np.cos(ang_other)],
+        A = np.array([[-np.cos(ang_track), np.cos(ang_other)],
                       [-np.sin(ang_track), np.sin(ang_other)]])
 
-        """
-        print('View',track.view, '->',track.path[0][0], ", ", track.path[0][1], ' to ', track.path[-1][0], ", ", track.path[-1][1])
-        print('with')
-        print('View',other.view,'->',other.path[0][0], ", ", other.path[0][1], ' to ', other.path[-1][0], ", ", other.path[-1][1])
-        """
 
         D = A[0,0]*A[1,1]-A[0,1]*A[1,0]
-        """
-        print(A)
-        print('D=',D)
-        """
         if(D == 0.):
             print("MEGA PBM :::  DETERMINANT IS ZERO")
             continue
@@ -200,24 +191,21 @@ def complete_trajectories(tracks):
             if(z >= z_o_min and z <= z_o_max):
                 pos_spl = float(spline(z))
                 a1t = float(deriv(z))
-                #a1 = 0. if a1 == 0 else 1./a1
+
             elif(z < z_o_min):
                 pos_spl = linear_interp(z-z_o_min, pos_o_min, deriv_z_min)
-                a1t = deriv_z_min #0. if deriv_z_min == 0 else 1./deriv_z_min
+                a1t = deriv_z_min 
 
             elif(z > z_o_max):
                 pos_spl = linear_interp(z-z_o_max, pos_o_max, deriv_z_max)
-                a1t = deriv_z_max #0. if deriv_z_max == 0 else 1./deriv_z_max
+                a1t = deriv_z_max 
 
 
 
             xy = A.dot([pos_spl, pos])/D
             x, y = xy[0], xy[1]
 
-            #print(x,y,z)
-
             dxdy = A.dot([a1t, a0t])/D
-
             dxdz, dydz = dxdy[0], dxdy[1]
 
 
@@ -225,63 +213,19 @@ def complete_trajectories(tracks):
             a1 = 0. if dydz == 0 else 1/dydz
 
 
-            ux = 1./math.sqrt(1. + pow(a0, 2)*(1./pow(a1, 2) + 1.)) if a1!=0 else 0.
-            uy = 1./math.sqrt(1. + pow(a1, 2)*(1./pow(a0, 2) + 1.)) if a0 !=0 else 0.
+            ux =  -1.*np.sign(a0)/math.sqrt(1. + pow(a0, 2)*(1./pow(a1, 2) + 1.)) if a1!=0 else 0.
+            uy =  -1.*np.sign(a1)/math.sqrt(1. + pow(a1, 2)*(1./pow(a0, 2) + 1.)) if a0 !=0 else 0.
 
-            cosgamma = math.fabs(np.sin(ang_track)*ux+np.cos(ang_track)*uy)
+            cosgamma = math.fabs(np.sin(ang_track-np.pi)*ux - np.cos(ang_track-np.pi)*uy)
                 
 
             dr = cf.view_pitch[v_track]/cosgamma if cosgamma != 0 else 9999.
 
-
-            """
-            #this was somewhat wrong with diagonal view 
-            #pxy = A.dot([0., cf.view_pitch[v_track]])/D
-            pitchx, pitchy = cf.view_pitch[v_track]*np.sin(ang_track), cf.view_pitch[v_track]*np.cos(ang_track)
-
-            dr = 1
-
-            if(a1 == 0):
-                dr *= math.sqrt(1. + pow(a0,2))
-            else :
-                dr = np.sqrt(pow(pitchx*math.sqrt(1. + pow(a0, 2)*(1./pow(a1, 2) + 1.)),2)) + np.sqrt(pow(pitchy*math.sqrt(1. + pow(a1, 2)*(1./pow(a0, 2) + 1.)),2))
-            """
-
-            
-            """ debugging """
-            if(p>999999):
-                print(p, " at z ", z, " : ", pos, " with ", pos_spl)
-                print(' matching v',v_track, ' with v',v_other)
-                print("  -> x ", x, " y ", y)
-                
-                print("   before rotation : ", a0t, " ", a1t)
-                print("   rotated ", dxdz, " ", dydz)
-                print("   pitches ", pitchx, " ",pitchy)
-                print("With prev point  : ", np.sqrt(pow(x-xp,2)+pow(y-yp,2) +pow(z-zp,2)))
-                print(" with prev dx ", x-xp, " dy ", y-yp, " dz ", z-zp)
-                dd = np.sqrt(pow(x-xp,2)+pow(y-yp,2)+pow(z-zp,2))
-                print(" ux = ",(x-xp)/dd, ' uy = ', (y-yp)/dd, ' uz = ', (z-zp)/dd)
-                print(" with prev pos : ", pos-pp)
-                print("   -->a0 ", a0, " a1 ", a1, " -> ds ", dr)
-                print('TEST')
-                
-                ux = 1./math.sqrt(1. + pow(a0, 2)*(1./pow(a1, 2) + 1.)) if a1!=0 else 0.
-                uy = 1./math.sqrt(1. + pow(a1, 2)*(1./pow(a0, 2) + 1.)) if a0 !=0 else 0.
-                cosgamma1 = math.fabs(np.sin(ang_track-np.pi/2.)*ux+np.cos(ang_track-np.pi/2.)*uy)
-                cosgamma2 = math.fabs(np.sin(ang_track)*ux+np.cos(ang_track)*uy)
-                
-                drtest1 = cf.view_pitch[v_track]/cosgamma1
-                drtest2 = cf.view_pitch[v_track]/cosgamma2
-                print('->ux',ux, 'uy',uy, " -> ", cosgamma1, cosgamma2)
-                print('----->',drtest1, drtest2)
-
-
-                print("----------")
-                xp = x
-                yp = y
-                zp = z
-                pp = pos
-
+            if(v_track >2):
+                print('----- at z=', z)
+                print(v_track, " at ", pos, " with ", v_other, " at ", pos_spl)
+                print("%.3f, %.3f"%(x, y))
+                print('PITCH : %.2f'%dr)
 
             trajectory.append( (x,y,z) )
             dQ.append(track.dQ[p])