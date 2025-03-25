bec9e760bc56404a15949619b8f00066153e6c88
Laura Zambelli <laura.zambelli@lapp.in2p3.fr>
2025-03-25 15:41:45 +0100

    cleaner code

--- lardon.py
+++ lardon.py
@@ -6,6 +6,7 @@ import det_spec as det
 import tables as tab
 import time as time
 
+from psutil import Process
 
 print("\nWelcome to LARDON !\n")
 
@@ -17,7 +18,7 @@ parser.add_argument('-run', help='Run number to be processed', required=True)
 parser.add_argument('-sub', help='Subfile to read', type=str, required=True)
 parser.add_argument('-n', '--nevent', type=int, help='number of events to process in the file [default (or -1) is all]', default=-1)
 
-parser.add_argument('-det', dest='detector', help='which detector is looked at [default is coldbox]', default='cb', choices=['cb1top', 'cb1bot','dp', 'cbtop', 'cbbot', '50l'])
+parser.add_argument('-det', dest='detector', help='which detector is looked at [default is coldbox]', default='cb', choices=['cb1top', 'cb1bot','dp', 'cbtop', 'cbbot', '50l', 'pdhd', 'pdvd'])
 parser.add_argument('-out', dest='outname', help='extra name on the output', default='')
 
 parser.add_argument('-skip', dest='evt_skip', type=int, help='nb of events to skip', default=0)
@@ -36,6 +37,8 @@ parser.add_argument('-pds', dest='do_pds', action='store_true', help='Flag that 
 
 parser.add_argument('-trk', dest='do_charge', action='store_true', help='Flag that lardon is reconstructing the charge data')
 
+parser.add_argument('-hash', dest='hash_path', type=str, default='', help='data hashed directories')
+
 args = parser.parse_args()
 
 print('Looking at ',args.detector, ' data' )
@@ -50,6 +53,8 @@ evt_skip = args.evt_skip
 do_pds = args.do_pds
 do_charge = args.do_charge
 
+hash_path = args.hash_path
+#print('hash: ', hash_path)
 
 if(do_pds == False and do_charge == False):
     print('Nothing asked to be reconstructed, please set -trk and/or -pds when you call LARDON')
@@ -60,7 +65,7 @@ else:
     
 
 
-det.configure(detector, run, do_pds)
+det.configure(detector, run, do_pds, hash_path)
 
 is_pulse = args.is_pulse
 if(is_pulse == True):
@@ -84,17 +89,9 @@ if(is_job == False):
     plot.set_style()
 
 import reconstruction_parameters as params
-import pedestals as ped
-import noise_filter as noise
-import store as store
-import hit_finder as hf
-import track_2d as trk2d
-import track_3d as trk3d
-import single_hits as sh
-import ghost as ghost
-import clusters as clu
-import matching as mat
 
+
+import workflow as work
 
 
 """ Special case when BDE-DAQ has to use multiple dataflow/datawriter app to write the data (e.g. high trigger rate, long window) """
@@ -129,15 +126,32 @@ if(is_job == False):
 else:
     name_out = f"{detector}_{run}_{sub}{multipass_daqname}{outname_option}.h5"
 
-print('Output name is : ', name_out)
+
+
+""" set analysis parameters """
+params.build_default_reco()
+params.configure(detector)
+#params.dump()
+
+    
+print('Output file is : ', name_out)
 output = tab.open_file(name_out, mode="w", title="Reconstruction Output")
+
+import store as store
+
 
 
 if(is_pulse):
     print('This is PULSING data reconstruction. Pulses will be found and fitted')
     print('WARNING : IT TAKES A LOT OF TIME')
     import pulse_waveforms as pulse
+    dc.set_waveforms()
     store.create_tables_pulsing(output)
+
+elif(detector == 'pdvd'):
+    store.create_tables_commissioning(output)
+    print('THIS IS TEMPORARY SIMPLE RECO CHANGE WHEN CATHODE IS ON')
+    
 else:
     store.create_tables(output)
 
@@ -148,23 +162,23 @@ if(do_pds):
 
 
     
-""" set analysis parameters """
-params.build_default_reco()
-params.configure(detector)
-#params.dump()
 
 
 """ set the channel mapping """
+
 cmap.get_mapping(detector)
-cmap.set_unused_channels()
+#cmap.set_unused_channels()
 
 
 
 
 """ setup the decoder """
-reader = decoder.decoder(detector, run, str(sub), dataflow+"-"+datawriter, args.file)
+reader = decoder.decoder(detector, run, str(sub), dataflow+"-"+datawriter, hash_path, args.file)
 reader.open_file()
 nb_evt = reader.read_run_header()
+
+
+
 
 single_event = args.single_event
 if(single_event >= 0):
@@ -196,19 +210,28 @@ else:
 store.store_run_infos(output, int(run), str(sub), nevent, time.time())
 store.save_reco_param(output)
 
+'''
 if(do_charge):
     store.store_chan_map(output)
+'''
 
 if(do_pds):
     store.store_pds_infos(output, int(run), str(sub), nevent, time.time())
-    
+
+
+deb = dc.debug()
+store.create_table_debug(output)
+
 for ievent in range(nevent):
 
     t0 = time.time()
+    ini_mem = Process().memory_info().rss
+
+    
     if(evt_skip > 0 and ievent < evt_skip):
         continue
 
-    dc.reset_event()
+    dc.reset_evt()
 
     print("-*-*-*-*-*-*-*-*-*-*-")
     print(" READING EVENT ", ievent)
@@ -216,204 +239,102 @@ for ievent in range(nevent):
     
     reader.read_evt_header(ievent)
     dc.evt_list[-1].dump()
+    
 
-
-    """ Decode the charge data and equalize the pedestals"""
-    if(do_charge):
-        reader.read_evt(ievent)
-
-        if(cf.n_sample <= 0):
-            print(' EVENT HAS NO CHARGE SAMPLE ...')
-            cf.n_sample = 0 #will be changed at the next event
-            store.store_event(output)
-            
-        else:
-            """ mask the unused channels """
-            dc.mask_daq = dc.alive_chan
-            
-            """ compute the raw pedestal """
-            """ produces a rough mask estimate """
-            ped.compute_pedestal(noise_type='raw')
-            
-            """ update the pedestal """
-            ped.compute_pedestal(noise_type='filt')
-            
-
-    ''' decode the light data and equalize the pedestals '''
+    ''' Workflow for PDS '''
     if(do_pds == True):       
-        reader.read_pds_evt(ievent)
+        dc.reset_event_pds()
+        reader.read_containers_evt(ievent)
 
         if(cf.n_pds_sample <=0):
             print(' EVENT HAS NO PDS SAMPLE ...')
             cf.n_pds_sample = 0
             store.store_pds_event(output)
             
-        else:
-            """ compute the pedestal """
-            ped.compute_pedestal_pds()
 
+        work.pds_signal_proc()
+        work.pds_reco()            
+                       
 
-    
-    if(do_charge and cf.n_sample > 0):
-        #plot.event_display_per_view([-100,100],[-10, 150],option='raw', to_be_shown=True) 
-        #plot.plot_wvf_current_vch([(0,750),(1,750),(2,750)], adc_min=-1, adc_max=-1, tmin=0, tmax=cf.n_sample, option='rawbnl', to_be_shown=True)
-        
-        if(is_pulse==True):
-            """ pulse analysis does not need noise filtering """
-            pulse.find_pulses()
-    
-            store.store_event(output)
-            store.store_pedestals(output)
-            store.store_pulse(output)
-            #store.store_avf_wvf(output)
-            continue
+    """ Workflow for charge """
+    if(do_charge == True):
 
-        ''' Perform the charge noise filtering '''
-        
-        """ low pass FFT cut """
-        ps = noise.FFT_low_pass(False)
-    
-        """ WARNING : DO NOT STORE ALL FFT PS !! """
-        #store.store_fft(output, ps)
-        #plot.plot_FFT_vch(ps, to_be_shown=True)
-    
-        #plot.event_display_per_view([-100,100],[-10, 150],option='fftbnl', to_be_shown=True)
-        #plot.plot_wvf_current_vch([(0,750),(1,750),(2,750)], adc_min=-1, adc_max=-1, tmin=0, tmax=cf.n_sample, option='fftbnl', to_be_shown=True)
-        
-        if(dc.data_daq.shape[-1] != cf.n_sample):
-            """ 
-            when the nb of sample is odd, the FFT returns 
-            an even nb of sample. 
-            Need to append an extra value (0) at the end 
-            of each waveform to make it work """
+        for imodule in cf.module_used:
             
-            dc.data_daq = np.insert(dc.data_daq, dc.data_daq.shape[-1], 0, axis=-1)
+            cf.imod = imodule
+            dc.reset_containers_trk()
 
+            mod_time = time.time()
+            t1 = time.time()
 
-        for n_iter in range(2):
-            ped.compute_pedestal(noise_type='filt')
-            ped.refine_mask(n_pass=2)
-
-        
-        """ special microphonic noise study """
-        ped.study_noise()
-
-    
-        """ CNR """
-        noise.coherent_noise()
-
-        #plot.event_display_per_view([-100,100],[-10, 150],option='cnrbnl', to_be_shown=True)
-        #plot.plot_wvf_current_vch([(0,750),(1,750),(2,750)], adc_min=-1, adc_max=-1, tmin=0, tmax=cf.n_sample, option='cnrbnl', to_be_shown=True)
-        """ microphonic noise """
-        noise.median_filter()
-
-        """ finalize pedestal RMS and ROI """
-        ped.compute_pedestal(noise_type='filt')
-        ped.refine_mask(n_pass=2)
-        ped.compute_pedestal(noise_type='filt')
-        #plot.event_display_per_view([-100,100],[-10, 150],option='microbnl', to_be_shown=True) 
-        #plot.plot_wvf_current_vch([(0,750),(1,750),(2,750)], adc_min=-1, adc_max=-1, tmin=0, tmax=cf.n_sample, option='microbnl', to_be_shown=True)
-        
-        #plot.event_display_per_view_noise([-100,100],[-50, 150],option='noise_filt', to_be_shown=True)
-        #plot.event_display_per_view([-100,100],[-10, 150],option='filt', to_be_shown=True) 
-        #plot.event_display_per_view_roi(adc_ind=[-100,100], adc_coll=[-10,150], option='fit_roibnl', to_be_shown=True)
-    
-        hf.find_hits()    
-        print("----- Number Of Hits found : ", dc.evt_list[-1].n_hits)
-        #plot.event_display_per_view([-100,100],[-10, 150],option='filt', to_be_shown=True) 
-        #plot.event_display_per_view_hits_found([-100,100],[-10, 150],option='hitsbnl', to_be_shown=True)    
-
-        #plot.plot_2dview_hits(to_be_shown=True)
-        #plot.plot_wvf_current_hits_roi_vch([(2,834)], tmin=5335, tmax=5435, option='coll_double_2bnl', to_be_shown=True)
-        #plot.plot_wvf_current_hits_roi_vch([(2,400)], tmin=6425, tmax=6550, option='coll_single_2bnl', to_be_shown=True)
-        #plot.plot_wvf_current_hits_roi_vch([(0,757)], tmin=4050, tmax=4300, option='ind_double_2bnl', to_be_shown=True)
-        #plot.plot_wvf_current_hits_roi_vch([(1,620)], tmin=1250, tmax=1500, option='ind_double_3bnl', to_be_shown=True)
-        #plot.plot_wvf_current_hits_roi_vch([(0, 250)], tmin=1800, tmax=2150, option='ind_single_bnl', to_be_shown=True)
-        #plot.plot_cumsum_wvf([(0,757)], tmin=4050, tmax=4300, option='ind_double_cumsum2bnl', to_be_shown=True)
-        #plot.plot_cumsum_wvf([(0, 250)], tmin=1800, tmax=2150, option='ind_single_cumsumbnl', to_be_shown=True)
-        #plot.plot_cumsum_wvf([(1,620)], tmin=1250, tmax=1500, option='ind_double_cumsum3bnl', to_be_shown=True)
-        
-        trk2d.find_tracks_rtree()
-        print("---- Number Of 2D tracks found : ", dc.evt_list[-1].n_tracks2D)
-
-        ghost.ghost_finder(threshold=10)
-        trk3d.find_tracks_rtree()    
-        ghost.ghost_trajectory()
-        
-        sh.single_hit_finder()
-
-        '''
-        for x in  dc.single_hits_list:
-            x.dump()
-            ids = x.IDs
-            hits = [dc.hits_list[x-dc.n_tot_hits] for hh in ids for x in hh]
-            daqchs = [h.daq_channel for h in hits]
-            #charges = [h.charge_pos for h in hits]
-            start, stop = min(x.start), max(x.stop)
-            """
-            for h in hits:
-                print(h.ID, ':', h.view, h.channel, ' -> ', h.charge, h.charge_pos, h.max_adc, ' from ', h.start, ' to ', h.stop)
-                print('integral from ', h.pad_start, ' to ', h.pad_stop)
-                print('test=', np.sum(dc.data_daq[h.daq_channel,h.pad_start:h.pad_stop]))
-            """
-            #if(x.veto[0]==False and x.veto[1]==False and x.veto[2]==False):
-                #plot.plot_wvf_current_daqch(daqchs, option='sh_'+str(x.ID_SH), to_be_shown=True, tmin=start, tmax=stop)
-        plot.event_display_per_view_hits_found([-400,400],[-10, 600],option='hits', to_be_shown=True)            
-        '''
-        #if(len(dc.tracks3D_list) > 0):
-            #[t.dump() for t in dc.tracks3D_list]
-            #plot.plot_2dview_hits_3dtracks(to_be_shown=True)
-            #plot.event_display_per_view_hits_found([-400,400],[-10, 600],option='hits', to_be_shown=True)            
-            #plot.plot_3d(to_be_shown=True)
-            #plot.charge_pds_zoom([0,1], (138, 152), (7580, 7700), option=None, to_be_shown=True)
-            #plot.event_display_coll_pds(draw_trk_t0 = True, to_be_shown=True)
+            reader.read_evt(ievent)
+            deb.read_data[cf.imod] = time.time()-t1
             
+            t1 = time.time()
+            if(cf.n_sample[cf.imod] <= 0):
+                print(' EVENT HAS NO CHARGE SAMPLE ...')
+                cf.n_sample = 0 #will be changed at the next event
+                store.store_event(output)
 
-        print("--- Number of 3D tracks found : ", len(dc.tracks3D_list))
-        print('-- Found ', len(dc.single_hits_list), ' Single Hits!')
-        print('- Found ', len(dc.ghost_list), ' Ghosts!')
-        print('%.2f s to process '%(time.time()-t0))
+            
+            if(is_pulse==True):
+                work.charge_pulsing()
+                continue
+            
+            work.charge_signal_proc(deb)
+                        
+            if(detector == 'pdvd'):
+                ''' temporary workflow for PDVD data '''
+                work.charge_reco_pdvd(deb)                
+            else:
+                work.charge_reco(deb)
 
-        
-    ''' WORKFLOW FOR THE LIGHT RECONSTRUCTION '''
-    if(do_pds and cf.n_pds_sample > 0):                
-        #plot.event_display_coll_pds(draw_trk_t0 = True, to_be_shown=True)
-        hf.find_pds_peak()
-        #plot.draw_pds_ED(to_be_shown=True)#, draw_peak=False, draw_cluster=False, draw_roi=False)
-        #[p.dump() for p in dc.pds_peak_list]
-        
-        clu.light_clustering()
-        print('--> Found ', dc.evt_list[-1].n_pds_clusters, ' clusters ')
-        #[c.dump() for c in dc.pds_cluster_list]
-        #plot.draw_pds_ED(to_be_shown=True, draw_peak=True, draw_cluster=True, draw_roi=False)
+            """ debugging tools """
+            curr_mem = Process().memory_info().rss
+            deb.memory_mod[cf.imod] = curr_mem
+            deb.time_mod[cf.imod] = time.time()-mod_time
+            
+            #plot.plot_2dview_2dtracks([cf.imod], to_be_shown=True)
 
     
-        if(do_charge and  cf.n_sample > 0):
-                mat.matching_charge_pds()                
+        
+    if(do_charge and do_pds):
+        work.match_charge_and_pds()
 
                 
 
-
+    t1 = time.time()
     """ store the results """
-    if(do_charge and cf.n_sample > 0):
+    if(do_charge and cf.n_sample[cf.imod] > 0):
         store.store_event(output)
         store.store_pedestals(output)
         store.store_noisestudy(output)
         store.store_hits(output)
-        store.store_tracks2D(output)
-        store.store_tracks3D(output)
-        store.store_single_hits(output)
-        store.store_ghost(output)
 
+        if(is_pulse==True):                      
+            store.store_event(output)
+            store.store_pedestals(output)
+            store.store_pulse(output)
+            #store.store_avf_wvf(output)
+
+        else:        
+            if(detector == 'pdvd'):
+                store.store_hits_3d(output)
+            else:
+                store.store_tracks2D(output)
+                store.store_tracks3D(output)
+                store.store_single_hits(output)
+                store.store_ghost(output)
+        
     if(do_pds and cf.n_pds_sample > 0):
         store.store_pds_event(output)
         store.store_pds_pedestals(output)
         store.store_pds_peak(output)
 
-        if(do_charge and  cf.n_sample > 0):
+        if(do_charge and  cf.n_sample[cf.imod] > 0):
             store.store_pds_cluster(output)
-
-                
+    deb.output = time.time()-t1
+        
     dc.n_tot_hits  += sum(dc.evt_list[-1].n_hits)
     dc.n_tot_pds_peaks += sum(dc.evt_list[-1].n_pds_peaks)    
     dc.n_tot_trk2d += sum(dc.evt_list[-1].n_tracks2D)
@@ -421,7 +342,15 @@ for ievent in range(nevent):
     dc.n_tot_ghosts += dc.evt_list[-1].n_ghosts
     dc.n_tot_sh += dc.evt_list[-1].n_single_hits
     dc.n_tot_pds_clusters += dc.evt_list[-1].n_pds_clusters
+    dc.n_tot_hits_clusters += dc.evt_list[-1].n_hits_clusters
 
+    end_mem = Process().memory_info().rss
+    deb.memory_tot = end_mem
+    deb.time_tot = time.time()-t0
+
+    #deb.dump()
+    store.store_debug(output, deb)
+    
 if(is_pulse==True):
     store.store_avf_wvf(output)
 
@@ -439,4 +368,6 @@ print('* Nb of Single Hits', dc.n_tot_sh)
 print('* Nb of PDS Peaks', dc.n_tot_pds_peaks)
 print('* Nb of PDS Clusters', dc.n_tot_pds_clusters)
 print('**************')
+
 print('it took %.2f s to run'%(time.time()-tstart))
+
