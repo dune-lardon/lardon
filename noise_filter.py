fd5daa2ed91a838168b7c0a1371c329f1a0bd301
Laura Zambelli <laura.zambelli@lapp.in2p3.fr>
2025-03-25 16:02:41 +0100

    done module per module

--- noise_filter.py
+++ noise_filter.py
@@ -17,8 +17,8 @@ def FFT_low_pass(save_ps=False):
     lowpass_cut = dc.reco['noise']['fft']['low_cut']
     freq_cut    = dc.reco['noise']['fft']['freq']
 
-    n    = int(cf.n_sample/2) + 1
-    rate = cf.sampling #in MHz
+    n    = int(cf.n_sample[cf.imod]/2) + 1
+    rate = cf.sampling[cf.imod] #in MHz
 
     freq = np.linspace(0, rate/2., n)
 
@@ -36,7 +36,7 @@ def FFT_low_pass(save_ps=False):
 
     """get power spectrum (before cut)"""
     if(save_ps==True):
-        ps = 1/cf.n_sample * np.abs(fdata)
+        ps = 1/cf.n_sample[cf.imod] * np.abs(fdata)
         #ps = 10.*np.log10(np.abs(fdata)+1e-1) 
 
     
@@ -71,14 +71,14 @@ def regular_coherent_noise():#groupings):
     groupings = dc.reco['noise']['coherent']['groupings']
     
     for group in groupings:
-        if( (cf.n_tot_channels % group) > 0):
+        if( (cf.module_nchan[cf.imod] % group) > 0):
             print(" Coherent Noise Filter in groups of ", group, " is not a possible ! ")
             return
 
-        nslices = int(cf.n_tot_channels / group)
+        nslices = int(cf.module_nchan[cf.imod] / group)
         
-        dc.data_daq = np.reshape(dc.data_daq, (nslices, group, cf.n_sample))
-        dc.mask_daq = np.reshape(dc.mask_daq, (nslices, group, cf.n_sample))
+        dc.data_daq = np.reshape(dc.data_daq, (nslices, group, cf.n_sample[cf.imod]))
+        dc.mask_daq = np.reshape(dc.mask_daq, (nslices, group, cf.n_sample[cf.imod]))
 
         """sum data if mask is true"""
         with np.errstate(divide='ignore', invalid='ignore'):
@@ -94,8 +94,8 @@ def regular_coherent_noise():#groupings):
         dc.data_daq -= mean[:,None,:]
 
         """ restore original data shape """
-        dc.data_daq = np.reshape(dc.data_daq, (cf.n_tot_channels, cf.n_sample))
-        dc.mask_daq = np.reshape(dc.mask_daq, (cf.n_tot_channels, cf.n_sample))
+        dc.data_daq = np.reshape(dc.data_daq, (cf.module_nchan[cf.imod], cf.n_sample[cf.imod]))
+        dc.mask_daq = np.reshape(dc.mask_daq, (cf.module_nchan[cf.imod], cf.n_sample[cf.imod]))
 
 
 def coherent_noise_per_view():#groupings, capa_weight, calibrated):
@@ -111,38 +111,40 @@ def coherent_noise_per_view():#groupings, capa_weight, calibrated):
 
 
 
-    v_daq = np.empty((cf.n_tot_channels,cf.n_sample))
-    capa = np.ones((cf.n_tot_channels))
-    calib = np.ones((cf.n_tot_channels))
+    v_daq = np.empty((cf.module_nchan[cf.imod], cf.n_sample[cf.imod]))
+    capa = np.ones((cf.module_nchan[cf.imod]))
+    calib = np.ones((cf.module_nchan[cf.imod]))
 
-
-    for i in range(cf.n_tot_channels):
-        view = dc.chmap[i].view
+    daqch_start = cf.module_daqch_start[cf.imod]
+    
+    for i in range(cf.module_nchan[cf.imod]):
+        
+        view = dc.chmap[i+daqch_start].view
         if(view >= cf.n_view or view < 0):
             view = -1
 
         v_daq[i,:] = view
         if(capa_weight):
-            capa[i] = dc.chmap[i].capa
+            capa[i] = dc.chmap[i+daqch_start].capa
         if(calibrated):
-            calib[i] = dc.chmap[i].gain
+            calib[i] = dc.chmap[idaqch_start].gain
 
     
     dc.data_daq *= calib[:,None]
     dc.data_daq /= capa[:,None]
          
     for group in groupings:
-        if( (cf.n_tot_channels % group) > 0):
+        if( (cf.module_nchan[cf.imod] % group) > 0):
             print(" Coherent Noise Filter in groups of ", group, " is not a possible ! ")
             return
 
-    nslices = int(cf.n_tot_channels / group)
+    nslices = int(cf.module_nchan[cf.imod] / group)
         
-    dc.data_daq = np.reshape(dc.data_daq, (nslices, group, cf.n_sample))
-    dc.mask_daq = np.reshape(dc.mask_daq, (nslices, group, cf.n_sample))
+    dc.data_daq = np.reshape(dc.data_daq, (nslices, group, cf.n_sample[cf.imod]))
+    dc.mask_daq = np.reshape(dc.mask_daq, (nslices, group, cf.n_sample[cf.imod]))
 
 
-    v_daq = np.reshape(v_daq, (nslices, group, cf.n_sample))
+    v_daq = np.reshape(v_daq, (nslices, group, cf.n_sample[cf.imod]))
 
     
     for i in range(cf.n_view):
@@ -162,8 +164,8 @@ def coherent_noise_per_view():#groupings, capa_weight, calibrated):
 
 
     """ restore original data shape """
-    dc.data_daq = np.reshape(dc.data_daq, (cf.n_tot_channels, cf.n_sample))
-    dc.mask_daq = np.reshape(dc.mask_daq, (cf.n_tot_channels, cf.n_sample))
+    dc.data_daq = np.reshape(dc.data_daq, (cf.module_nchan[cf.imod], cf.n_sample[cf.imod]))
+    dc.mask_daq = np.reshape(dc.mask_daq, (cf.module_nchan[cf.imod], cf.n_sample[cf.imod]))
     dc.data_daq /= calib[:,None]
     dc.data_daq *= capa[:,None]
 
@@ -182,7 +184,7 @@ def median_filter():
     if(window < 0):
         return
 
-    if(window > cf.n_sample):
+    if(window > cf.n_sample[cf.imod]):
         return
     
     """ apply median filter on data to remove microphonic noise """    
@@ -190,8 +192,29 @@ def median_filter():
     """ mask the data with nan where potential signal is (ROI)"""
     med = centered_median_filter(np.where(dc.mask_daq==True, dc.data_daq, np.nan), window)
 
-    """ in median computation, if everything is masked (all nan) nan is returnedso changed these cases to 0 """
+    """ in median computation, if everything is masked (all nan) nan is returned, so to fixe these cases to 0 """
     med = np.nan_to_num(med, nan=0.)
 
     """ apply correction to data points """
     dc.data_daq -= med
+
+
+def median_filter_pds():
+    window = 4000
+    
+    if(window < 0):
+        return
+    
+    if(window > cf.n_pds_sample):
+        return
+    
+    """ apply median filter on data to remove microphonic noise """    
+
+    """ mask the data with nan where potential signal is (ROI)"""
+    med = centered_median_filter(np.where(dc.mask_pds==True, dc.data_pds, np.nan), window)
+
+    """ in median computation, if everything is masked (all nan) nan is returned, so to fixe these cases to 0 """
+    med = np.nan_to_num(med, nan=0.)
+
+    """ apply correction to data points """
+    dc.data_pds -= med
