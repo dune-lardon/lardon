bda5b527fc39f89df13cc17eda53ae606c2e1908
Laura Zambelli <laura.zambelli@lapp.in2p3.fr>
2026-09-04 21:28:58 +0200

    more info and better light clustering

--- src/lardon/clusters.py
+++ src/lardon/clusters.py
@@ -14,25 +14,41 @@ import time
 
 def build_pds_cluster(peaks, idx):
     cluster_ID = idx
+    """ sort peaks by global channel number """
+    peaks.sort()
+    
     IDs = [p.ID for p in peaks]
     glob_chans = [p.glob_ch for p in peaks]
     channels = [p.channel for p in peaks]
-    t_starts = [p.start for p in peaks]
-    t_maxs = [p.max_t for p in peaks]
-    t_stops = [p.stop for p in peaks]
+
+    #t_starts = [p.start for p in peaks]
+    #t_maxs = [p.max_t for p in peaks]
+    #t_stops = [p.stop for p in peaks]
+
+    
     max_adcs = [p.max_adc for p in peaks]
     charges = [p.charge for p in peaks]
-    timestamps = [p.timestamp for p in peaks]
-    timestamp = min(timestamps)
 
-    cluster = dc.pds_cluster(cluster_ID, IDs, glob_chans, channels, t_starts, t_maxs, t_stops, max_adcs, charges, timestamp)    
+    max_npes = [p.max_npe for p in peaks]
+    npes = [p.npe for p in peaks]
+
+
+    timestamps = [p.timestamp for p in peaks]
+    durations = [p.timestamp + (p.stop-p.start)/cf.pds_sampling for p in peaks]
+    
+    timestamp = min(timestamps)
+    timestamp_end = max(durations)
+    
+    #cluster = dc.pds_cluster(cluster_ID, IDs, glob_chans, channels, t_starts, t_maxs, t_stops, max_adcs, charges, timestamp)
+
+    cluster = dc.pds_cluster(cluster_ID, IDs, glob_chans, channels, max_adcs, charges, max_npes, npes, timestamp, timestamp_end)
     [p.set_cluster_ID(idx) for p in peaks]
 
     return cluster
 
 
 
-def sparse_crosscorr(peaks_ref, peaks_ch, max_lag):
+def sparse_crosscorr(peaks_ref, peaks_ch, max_lag, debug=False):
     """
     peaks_ref and peaks_ch are sorted lists/arrays of integer time bins.
     max_lag is the search window (in bins).
@@ -46,7 +62,8 @@ def sparse_crosscorr(peaks_ref, peaks_ch, max_lag):
     # For each lag, count overlaps
     for i, lag in enumerate(lag_range):
         shifted = peaks_ch + lag/cf.pds_sampling
-
+        if(debug):
+            print(i, lag/cf.pds_sampling, shifted[:30])
         lag_scores[i] = np.sum(np.isin(shifted, peaks_ref))
 
     best_lag = lag_range[np.argmax(lag_scores)]
@@ -65,29 +82,38 @@ def align_waveforms(max_lag):
 
 
     [peaks_time[ch].sort() for ch in range(cf.n_pds_tot_channels)]
-
+    delays = [0 for x in range(cf.n_pds_tot_channels)]
 
     reference = peaks_time[0]
+    if(len(reference) == 0):
+        return
     dc.evt_list[-1].pds_time_offset[0] = 0.
-
     
-    delays = [0 for x in range(cf.n_pds_tot_channels)]
+
     for ch in range(cf.n_pds_tot_channels):
         if ch == 0:
             delays[ch] = 0
             continue
 
-        lag, score = sparse_crosscorr(reference, peaks_time[ch], max_lag)
+        lag, score = sparse_crosscorr(reference, peaks_time[ch], max_lag)#,debug=ch==1)
         delays[ch] = lag
 
         if(score > 0):
             dc.evt_list[-1].pds_time_offset[ch] = lag/cf.pds_sampling
-    
+    #print("DELAY: ", dc.evt_list[-1].pds_time_offset)
+
+def check_unique_channels(peaks):
+    """ remove all peaks in a given channel if they appear multiple time in the cluster """
+    gchan = [p.glob_ch for p in peaks]
+    counts = Counter(gchan)
+    unique_chan = [i for i, x in enumerate(gchan) if counts[x] == 1]
+    return [peaks[i] for i in unique_chan]
+
 def light_clustering():
 
     max_lag = dc.reco['pds']['cluster']['max_lag'] #in tick nb
     time_tol = dc.reco['pds']['cluster']['time_tol'] #in mus
-    
+
     align_waveforms(max_lag)
     
     cluster_list = []
@@ -121,9 +147,8 @@ def light_clustering():
     for pi in dc.pds_peak_list:
         if(pi.cluster_ID >= 0):
             continue
-        
-        i_start = pi.timestamp + dc.evt_list[-1].pds_time_offset[chan]
-        i_chan  = pi.glob_ch
+        i_chan  = pi.glob_ch        
+        i_start = pi.timestamp + dc.evt_list[-1].pds_time_offset[i_chan]
         i_ID    = pi.ID 
         i_mod   = pi.module
             
@@ -135,11 +160,13 @@ def light_clustering():
                 ov_idx = ov - id_peak_shift
                 pj = dc.pds_peak_list[ov_idx]
                                 
-                if (pj.cluster_ID >=0 or pj.ID == i_ID or pj.glob_ch == chan):
+                if (pj.cluster_ID >=0 or pj.ID == i_ID):# or pj.glob_ch == i_chan):
                     continue
             
                 peaks.append(pj)
-                    
+
+
+            peaks = check_unique_channels(peaks)
             if(len(peaks) > 1):
                 ID = len(dc.pds_cluster_list) + id_cluster_shift
                 clus = build_pds_cluster(peaks, ID)
@@ -147,139 +174,6 @@ def light_clustering():
                 dc.evt_list[-1].n_pds_clusters += 1
         
 
-
-
-
-
-
-
-def old_light_clustering():
-
-    cluster_list = []
-    
-    if(len(dc.pds_peak_list) <2 ):
-        return
-
-    time_tol = dc.reco['pds']['cluster']['time_tol'] #in ticks
-    print('TiME TOLERANCE CLUSTERING ', time_tol)
-    
-    id_peak_shift = dc.n_tot_pds_peaks
-    id_cluster_shift = dc.n_tot_pds_clusters
-
-    
-    pties = index.Property()
-    pties.dimension = 3
-
-    ''' create an rtree index (channel, pds peak times)'''
-    rtree_idx = index.Index(properties=pties)
-
-    ''' filling the R-tree '''
-    for p in dc.pds_peak_list:
-
-        start = p.start
-        chan  = p.glob_ch
-        readout = chan%2
-        module  = p.module
-        ID    = p.ID
-
-        rtree_idx.insert(ID, (readout, module, start, readout, module, start))        
-        
-        """ NB : this is for the coldbox only, as the membrane PDS had only one readout """
-        if(cf.pds_modules_type[module] == 'Membrane'):
-            ID = len(cluster_list) + id_cluster_shift
-            clus = build_pds_cluster([p, p], ID)
-            cluster_list.append( clus )
-            continue
-
-
-        
-
-    ''' Now searching for overlaps in the same PDS module'''
-    for pi in dc.pds_peak_list:
-        if(pi.cluster_ID >= 0):
-            continue
-        
-        i_start = pi.start
-        i_stop  = pi.stop
-        i_chan  = pi.glob_ch
-        i_readout = i_chan%2
-
-        if(i_readout != 0):
-            continue
-        
-        i_module  = int(i_chan/2)
-        i_ID    = pi.ID 
-    
-        j_readout = 1-i_readout
-        
-        overlaps = list(rtree_idx.intersection((j_readout, i_module, i_start - time_tol, j_readout, i_module, i_start + time_tol)))        
-        
-        if(len(overlaps) > 0):
-            peaks = [pi]
-            for ov in overlaps:
-                ov_idx = ov - id_peak_shift
-                pj = dc.pds_peak_list[ov_idx]
-                                
-                if (pj.cluster_ID >=0 or pj.ID == i_ID):
-                    continue
-            
-                peaks.append(pj)
-                    
-            if(len(peaks) > 1):
-                ID = len(cluster_list) + id_cluster_shift
-                clus = build_pds_cluster(peaks, ID)
-                cluster_list.append( clus )
-
-    
-
-    """ try to merge the clusters across the PDS """
-    """ create a new Rtree, now filled with the found clusters """
-    pties = index.Property()
-    pties.dimension = 2
-
-    ''' create an rtree index (times and 0)'''
-    rtree_mod = index.Index(properties=pties)
-
-    
-    ''' filling the R-tree '''
-    for c in cluster_list:        
-        start = c.t_start
-        idx   = c.ID
-        rtree_mod.insert(idx, (start, 0, start, 0))
-
-        
-    ''' now search for overlaps '''
-    for ci in cluster_list:        
-        if(ci.ID < 0):
-            """ this cluster has already been merged """
-            continue
-        
-        i_start = ci.t_start
-        i_idx   = ci.ID
-
-        overlaps = list(rtree_mod.intersection((i_start-time_tol, 0, i_start+time_tol, 0)))
-        
-        if(len(overlaps)>0):
-            for ov in overlaps:
-                if(ov <= i_idx):
-                    continue
-                co = cluster_list[ov-id_cluster_shift]
-            
-                ci.merge(co)
-                co.set_ID(-1)
-                
-                [dc.pds_peak_list[p-id_peak_shift].set_cluster_ID(i_idx) for p in ci.peak_IDs]
-
-        if(ci.ID >=0):
-            if(ci.ID == dc.evt_list[-1].n_pds_clusters+id_cluster_shift):
-               dc.pds_cluster_list.append(ci)
-               dc.evt_list[-1].n_pds_clusters += 1
-            else:
-               new_ID = dc.evt_list[-1].n_pds_clusters + id_cluster_shift
-               ci.set_ID(new_ID)
-               [dc.pds_peak_list[p-id_peak_shift].set_cluster_ID(new_ID) for p in ci.peak_IDs]
-               dc.pds_cluster_list.append(ci)
-               dc.evt_list[-1].n_pds_clusters += 1
     
 
 
